4b273036df2582b18191a7d3dcf619c4cedbfb5c
manelhr <manoelhortaribeiro@gmail.com>
2016-09-10 16:53:15 -0300

    Finishes to test the graph, implements undirected edge on the back-end.

--- backend/query_interpreter/graph_maker.py
+++ backend/query_interpreter/graph_maker.py
@@ -1,5 +1,4 @@
 class Graph:
-
     # Auxiliary global variable that will be used in the method to get all maximal paths.
     calculated_paths = []
 
@@ -14,8 +13,7 @@ class Graph:
         * (self.edges_aux = ) (source node_id, destination node_id): {edge content}
 
         :param nodes: list of hashes that contain an id field.
-        :param edges: list of hashes that contain information about two nodes.
-        """
+        :param edges: list of hashes that contain information about two nodes. """
 
         self.nodes, self.edges, self.edges_aux = dict(), dict(), dict()
 
@@ -31,8 +29,7 @@ class Graph:
     def get_node(self, number):
         """ This is an accessor to get a node based on its unique id.
         :param number: id of the node.
-        :return: the node itself.
-        """
+        :return: the node itself. """
 
         return self.nodes[number][0]
 
@@ -40,12 +37,13 @@ class Graph:
         """ This is an accessor to get an edge based the unique ids of its source or dest nodes.
         :param number1: id of the source node.
         :param number2: id of the destination node.
-        :return: the edge itself.
-        """
+        :return: the edge itself. """
 
         return self.edges_aux[(number1, number2)]
 
     def make_maximal_paths(self):
+        """ This function returns a list of the maximal paths of the graph.
+        :return: list containing the indexes of the maximal paths. """
 
         initial_nodes = []
         for i in self.nodes.keys():
@@ -60,15 +58,31 @@ class Graph:
         return paths
 
     @staticmethod
-    def get_all_paths(current_node, nodes, edges, acc=[]):
+    def get_all_paths(current_node, nodes, edges, acc=[], switch_last=False):
+        """ This function gets all paths possible starting from the given node. It also implements the undirected edge
+        feature, where a undirected edge: (a)--(b) is equivalent to orderings a,b and b,a.
+        :param current_node: current node to be inspected.
+        :param nodes: hash containing all nodes.
+        :param edges: hash containing all edges.
+        :param acc: accumulator.
+        :param switch_last: if we should switch the current node with the last in the accumulator or not.
+        :return: it saves the paths in the graph global variable.
+        """
 
         nacc = list(acc)
         nacc.append(current_node)
         edges_out = nodes[current_node][1]
+
+        if switch_last:
+            nacc[-1], nacc[-2] = nacc[-2], nacc[-1]
 
         if len(edges_out) == 0:
             Graph.calculated_paths.append(nacc)
             return
 
         for edge in edges_out:
-            Graph.get_all_paths(edges[edge]['dst']['id'], nodes, edges, nacc)
+            if edges[edge]['kind'] == 'directed':
+                Graph.get_all_paths(edges[edge]['dst']['id'], nodes, edges, nacc)
+            else:
+                Graph.get_all_paths(edges[edge]['dst']['id'], nodes, edges, nacc)
+                Graph.get_all_paths(edges[edge]['dst']['id'], nodes, edges, nacc, switch_last=True)
--- tests/test_query_interpreter/test_graph_maker.py
+++ tests/test_query_interpreter/test_graph_maker.py
@@ -19,6 +19,59 @@ class TestGraphMaker(unittest.TestCase):
 
         return nodes, edges
 
+    @staticmethod
+    def complex_graph():
+        nodes = [
+            {'className': 'Node', 'display_value': ['Diagnosis is HPV: Negative'],
+             'key_op_value': [['Diagnosis', '==', '1']], 'id': 0, 'name': 'n0'},
+            {'className': 'Node', 'display_value': ['Diagnosis is Cyt: Normal'],
+             'key_op_value': [['Diagnosis', '==', '11']], 'id': 1, 'name': 'n1'},
+            {'className': 'Node', 'display_value': ['Diagnosis is HPV: Negative'],
+             'key_op_value': [['Diagnosis', '==', '1']], 'id': 3, 'name': 'n3'},
+            {'className': 'Node', 'display_value': ['Diagnosis is Cyt: Normal'],
+             'key_op_value': [['Diagnosis', '==', '11']], 'id': 5, 'name': 'n5'},
+            {'className': 'Node', 'display_value': ['Diagnosis is Cyt: LSIL'],
+             'key_op_value': [['Diagnosis', '==', '13']], 'id': 7, 'name': 'n7'},
+            {'className': 'Node', 'display_value': ['Diagnosis is Cyt: Normal'],
+             'key_op_value': [['Diagnosis', '==', '11']], 'id': 9, 'name': 'n9'}
+        ]
+
+        edges = [
+            {'source': 'n0', 'className': 'Edge', 'display_value': [],
+             'dst': nodes[1],
+             'destination': 'n1', 'key_op_value': [],
+             'src': nodes[0],
+             'id': 2, 'name': 'e2', 'kind': 'directed'},
+
+            {'source': 'n3', 'className': 'Edge', 'display_value': [],
+             'dst': nodes[1],
+             'destination': 'n1', 'key_op_value': [],
+             'src': nodes[2],
+             'id': 4, 'name': 'e4', 'kind': 'directed'},
+
+            {'source': 'n5', 'className': 'Edge', 'display_value': [],
+             'dst': nodes[4],
+             'destination': 'n7', 'key_op_value': [],
+             'src': nodes[3],
+             'id': 8, 'name': 'e8', 'kind': 'directed'},
+
+            {'source': 'n5', 'className': 'Edge', 'display_value': [],
+             'dst': nodes[5],
+             'destination': 'n9', 'key_op_value': [],
+             'src': nodes[3],
+             'id': 10, 'name': 'e10', 'kind': 'directed'},
+
+            {'source': 'n1', 'className': 'Edge',
+             'display_value': ['Time Interval smaller than 365'],
+             'dst': nodes[3],
+             'destination': 'n5',
+             'key_op_value': [['TimeSinceLast', '<', '365']],
+             'src': nodes[1],
+             'id': 11, 'name': 'e11', 'kind': 'undirected'}
+        ]
+
+        return nodes, edges
+
     def test_graph_creator(self):
         nodes, edges = TestGraphMaker.simple_graph()
         graph = Graph(nodes, edges)
@@ -36,3 +89,11 @@ class TestGraphMaker(unittest.TestCase):
         self.assertEqual(graph.get_node(1), nodes[1])
         self.assertEqual(graph.get_edge(0, 1), edges[0])
         print(">> backend.query_interpreter.graph_maker.accessor - OK")
+
+    def test_graph_paths(self):
+        nodes, edges = TestGraphMaker.complex_graph()
+        graph = Graph(nodes, edges)
+        expected = [[0, 1, 5, 7], [0, 1, 5, 9], [0, 5, 1, 7], [0, 5, 1, 9],
+                    [3, 1, 5, 7], [3, 1, 5, 9], [3, 5, 1, 7], [3, 5, 1, 9]]
+        self.assertEqual(graph.make_maximal_paths(), expected)
+        print(">> backend.query_interpreter.graph_maker.maximal_paths - OK")
